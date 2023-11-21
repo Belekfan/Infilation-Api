4a4e6300baee2a487f10226911c432e327fb7185
Belekfan <151651778+Belekfan@users.noreply.github.com>
2023-11-21 17:47:08 +0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import os
+import json
+
+app = Flask(__name__)
+
+
+f = open('data.json')
+
+
+data = json.load(f)
+
+
+@app.route('/infilation')
+def send_inflation():
+  date = request.args.get('date')
+  for i in data:
+    if date==i['date']:
+      return jsonify(i)
+
+
+@app.route('/')
+def hello_world():
+  return "Hello World"
