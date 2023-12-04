0a3e562e2379e4f33f8a599c461b36d83cb68a1a
Belekfan <151651778+Belekfan@users.noreply.github.com>
2023-12-04 23:10:42 +0300

    Foreign trade added

--- api.py
+++ api.py
@@ -10,6 +10,10 @@ f = open('data.json')
 
 data = json.load(f)
 
+f2 = open('data2.json')
+
+
+data2 = json.load(f2)
 
 @app.route('/infilation')
 def send_inflation():
@@ -18,6 +22,13 @@ def send_inflation():
     if date==i["date"]:
       return jsonify(i)
 
+
+@app.route('/foreigntrade')
+def send_inflation():
+  date = request.args.get('date')
+  for i in data2["foregintrade"]:
+    if date==i["date"]:
+      return jsonify(i)
 
 @app.route('/')
 def hello_world():
